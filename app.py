94428bb40170cb3b078f7bb8326f4c9e9060e897
Anant Vardhan <anantvardhan.04@gmail.com>
2024-09-21 01:01:19 +0530

    refactor: moved parse events to seprate function

--- app.py
+++ app.py
@@ -1,22 +1,33 @@
-import json
 import requests
 import argparse
 
-def get_git_events(user):
+
+def parse_events(event: dict) -> None:
+    if event["type"] == "CreateEvent" and event["payload"]["ref_type"] == "repository":
+        print(f'- Created { event["repo"]["name"]} {event["payload"]["ref_type"]}')
+    elif event["type"] == "CreateEvent":
+        print(f'- Created { event["payload"]["ref"]} {event["payload"]["ref_type"]}')
+    elif event["type"] == "PushEvent" and event["payload"]["commits"]:
+        num_of_commits = len(event["payload"]["commits"])
+        print(f'- Pushed {num_of_commits} commit to {event["repo"]["name"]}')
+    
+def get_git_events(user: str, event_per_page: int = 10) -> None:
         url = f"https://api.github.com/users/{user}/events"
-        response = requests.get(url)
-        if response.status_code == 200:
-            event_json = response.json()
-            for event in event_json:
-                if event["type"] == "CreateEvent" and event["payload"]["ref_type"] == "repository":
-                    print(f'- Created { event["repo"]["name"]} {event["payload"]["ref_type"]}')
-                elif event["type"] == "CreateEvent":
-                    print(f'- Created { event["payload"]["ref"]} {event["payload"]["ref_type"]}')
-                elif event["type"] == "PushEvent" and event["payload"]["commits"]:
-                    num_of_commits = len(event["payload"]["commits"])
-                    print(f'- Pushed {num_of_commits} commit to {event["repo"]["name"]}')
-        else:
-            print(f"failed {response.status_code}")
+        page_number = 1
+        while True:
+            params = {"page": page_number, "per_page": event_per_page}
+            response = requests.get(url, params=params)
+            if response.status_code == 200:
+                event_json = response.json()
+                if not event_json:
+                    break
+                for event in event_json:
+                    parse_events(event)
+            else:
+                print(f"failed {response.status_code}")
+            page_number += 1
+
+
 
 def main():
     # create a parser
@@ -28,7 +39,6 @@ def main():
     if args.name:
         get_git_events(args.name)
         
-
 
 if __name__ == "__main__":
     main()
